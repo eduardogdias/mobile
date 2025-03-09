38ac021d7240fd1c73ee65f01b31586ee7e0e957
Eduardo Guilherme Dias <eduardogdias.edu@gmail.com>
2025-03-09 16:44:47 -0300

    aula2

---
+++ aula02_r/hello_world/App.js
@@ -0,0 +1,12 @@
+import {React} from 'react';
+import {View, Text} from 'react-native';
+
+function Principal () {
+  return(
+    <View style={{flex:1, justifyContent: "center", alignItems: "center", backgroundColor: "lightgreen"}}>
+      <Text style={{fontSize: 26}}>Hello World!</Text>
+    </View>
+
+  )
+}
+export default Principal;
